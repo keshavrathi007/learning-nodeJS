dc10f651bd356c3b0bd235d47aba058a2580d181
keshav rathi <keshavanilrathi21@gmail.com>
2021-05-06 17:13:54 +0530

    second commit

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -3,6 +3,17 @@ const fs = require("fs")
 var requests = require('requests');
 
 const homeFile = fs.readFileSync("home.html", "utf-8")
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+    return temperature;
+};
+
 const server = http.createServer((req, res) => {
 
     if (req.url == "/") {
@@ -10,12 +21,13 @@ const server = http.createServer((req, res) => {
             .on('data', (chunk) => {
                 const objData = JSON.parse(chunk)
                 const arrData = [objData]
-                console.log(arrData)
+                // console.log(arrData[0].main.temp)
+                const realTimeData = arrData.map(val => replaceVal(homeFile, val)).join(" ");
+                res.write(realTimeData)
             })
             .on('end', (err) => {
                 if (err) return console.log('connection closed due to errors', err);
-
-                console.log('end');
+                res.end()
             });
     }
 })
